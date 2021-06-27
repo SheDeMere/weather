f5521c935ebdad47fa1df617e41033bfdcdef74e
SheDeMere <apti2500@mail.ru>
2021-06-27 17:56:51 +0300

    'fix'

--- src/redux/action.jsx
+++ src/redux/action.jsx
@@ -3,7 +3,7 @@ export const loadWeather = (city) => {
     dispatch({ type: 'weather/load/start' });
 
     fetch(
-      `http://api.weatherapi.com/v1/current.json?key=65d6fa8cb55a47c0aa1133000212904&q=${city || 'grozny'}&aqi=no`,
+      `https://api.weatherapi.com/v1/current.json?key=65d6fa8cb55a47c0aa1133000212904&q=${city || 'grozny'}&aqi=no`,
     )
       .then((response) => response.json())
       .then((json) => {
